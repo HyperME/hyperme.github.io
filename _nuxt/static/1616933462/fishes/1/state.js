35efa2212d680cd542cee5c5f9f079b71a8b6913
HyperME <maxim.okolzin@gmail.com>
2021-03-28 15:26:48 +0300

    init fishinfo

---
+++ _nuxt/static/1616933462/fishes/1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616933462",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffishes\u002F1",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
